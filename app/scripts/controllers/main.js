c843c9ff99493ac72661d28afaba4f4d3a053f27
Travis Beatty <travisby@gmail.com>
2015-02-03 07:08:09 -0500

    Scaffolded pathfinder-utils

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pathfinderUtilsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pathfinderUtilsApp
+ */
+angular.module('pathfinderUtilsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ test/protractor.conf.js
@@ -0,0 +1,25 @@
+exports.config = {
+
+  // Spec patterns are relative to the location of this config.
+  specs: [
+    'spec/e2e/**/*.js'
+  ],
+
+
+  multiCapabilities: [{
+    'browserName': 'chrome'
+  }],
+
+
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+  baseUrl: 'http://localhost:9000',
+
+  jasmineNodeOpts: {
+    onComplete: null,
+    isVerbose: false,
+    showColors: true,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 10000
+  }
+};
